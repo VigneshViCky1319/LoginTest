26837ec45d8fed6b4c1ac1fa1b85d5055848ea2c
Jayasree <jayasree.suresh@accenture.com>
2019-12-05 12:32:56 +0530

    Version 4.0

--- src/test/java/testPack/onlineShopping.java
+++ src/test/java/testPack/onlineShopping.java
@@ -10,27 +10,25 @@ import org.testng.annotations.Test;
 
 public class onlineShopping {
 
-	WebDriver dir;
-	@Test
-	public void login()
-	{
-		
-		dir.findElement(By .xpath("//input[@name='userName']")).sendKeys("vicky");
-		dir.findElement(By .xpath("//input[@name='password']")).sendKeys("vicky");
-		dir.findElement(By .xpath("//input[@name='login']")).click();
-	}
-	@BeforeMethod
-	public void beforeMethod() {
+	WebDriver driver;
+	  @Test
+	  public void f() {
+		  driver.findElement(By.name("userName")).sendKeys("mercury");
+		  driver.findElement(By.name("password")).sendKeys("mercury");
+		  driver.findElement(By.xpath("//input[@value='Login']")).click();
+	  }
+	  @BeforeMethod
+	  public void beforeMethod() {
 		  //launch browser and application
-		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
-		WebDriver dri = new ChromeDriver();
-		dri.get("http://www.newtours.demoaut.com/");
+		  System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
+		  driver=new ChromeDriver();
+		  driver.get("http://newtours.demoaut.com");
 	  }
 
 	  @AfterMethod
 	  public void afterMethod() {
-		  Assert.assertTrue(dir.getTitle().contains("Flight"));
-		  dir.close();
+		  Assert.assertTrue(driver.getTitle().contains("Flight"));
+		  driver.close();
 	  }
 
 }
