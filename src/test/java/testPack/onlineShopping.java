5a8697efadd0aa87f7e1c3f23c20cf063fb346bf
Jayasree <jayasree.suresh@accenture.com>
2019-12-05 12:15:24 +0530

    Version 1.0

--- src/test/java/testPack/onlineShopping.java
+++ src/test/java/testPack/onlineShopping.java
@@ -3,19 +3,34 @@ package testPack;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class onlineShopping {
 
+	WebDriver dir;
 	@Test
 	public void login()
 	{
+		
+		dir.findElement(By .xpath("//input[@name='userName']")).sendKeys("vicky");
+		dir.findElement(By .xpath("//input[@name='password']")).sendKeys("vicky");
+		dir.findElement(By .xpath("//input[@name='login']")).click();
+	}
+	@BeforeTest
+	public void beforeTest() {
+		  //launch browser and application
 		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
 		WebDriver dri = new ChromeDriver();
 		dri.get("http://www.newtours.demoaut.com/");
-		dri.findElement(By .xpath("//input[@name='userName']")).sendKeys("vicky");
-		dri.findElement(By .xpath("//input[@name='password']")).sendKeys("vicky");
-		dri.findElement(By .xpath("//input[@name='login']")).click();
-	}
+	  }
+
+	  @AfterTest
+	  public void afterTest() {
+		  Assert.assertTrue(dir.getTitle().contains("Flight"));
+		  dir.close();
+	  }
 
 }
